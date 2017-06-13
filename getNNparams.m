4b77c3cf08fe39d620d950c90aefc0a3546f6d94
rib1 <rib1@DBH-133-opti9020_1.ad.rice.edu>
2017-06-13 15:04:14 -0500

    Added empty function getNNparams.

---
+++ getNNparams.m
@@ -0,0 +1,5 @@
+function [  ] = getNNparams( centerpt, ptslist )
+%getNNparams Sorts out points into a '+' shape with one point in the center
+%   INPUT:  centerpt - [x,y] row vector of central point
+%           ptslist - list of [x,y] row vectors of nearest-neighbors
+
