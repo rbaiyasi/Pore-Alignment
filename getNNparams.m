df50a70b72bcac796ea79d5606b5afd6c75c57ef
rib1 <rib1@DBH-133-opti9020_1.ad.rice.edu>
2017-06-13 16:06:56 -0500

    Completed getNNparams function. Initial tests show that it works when 3 or 4 nearest neighbors are passed to it.

--- getNNparams.m
+++ getNNparams.m
@@ -1,7 +1,13 @@
-function [  ] = getNNparams( centerpt, ptslist )
+function [ NNdists , NNangs , resortIdx ] = getNNparams( centerpt, ptslist )
 %getNNparams Sorts out points into a '+' shape with one point in the center
+%NOTE--results are sorted as right,left,up,down
 %   INPUT:  centerpt - [x,y] row vector of central point
 %           ptslist - list of [x,y] row vectors of nearest-neighbors
+%   OUTPUT: NNdists - pixel distances between each neighbor
+%           NNangs - angle in radians to each neighbor
+%           resortIdx - indices used to reassign points. Note that a dummy
+%               index (1 greater than the number of pts in ptslist) will be
+%               assigned to each missing point.
 %% Error Codes
 repeatPtError.message = 'A nearest-neigbor has been sorted into two positions';
 toomanyinPtError.message = 'More than one nearest-neighbor has been assigned to one position';
@@ -22,11 +28,40 @@ NNang = angle(NNdisps);
 % close to 45 degrees, this should be fine.
 % for example, the right nearest neigbor is the neighbor with a positive
 % real part that is larger than the imaginary part
-rNNidx = find(real(NNdisps) > 0 & abs(real(NNdisps)) >= abs(imag(NNdisps)));
-lNNidx = find(real(NNdisps) < 0 & abs(real(NNdisps)) >= abs(imag(NNdisps)));
-uNNidx = find(imag(NNdisps) > 0 & abs(real(NNdisps)) < abs(imag(NNdisps)));
-dNNidx = find(imag(NNdisps) < 0 & abs(real(NNdisps)) < abs(imag(NNdisps)));
+NNidxFields = {'r','l','u','d'};
+NNidx.r = find(real(NNdisps) > 0 & abs(real(NNdisps)) >= abs(imag(NNdisps)));
+NNidx.l = find(real(NNdisps) < 0 & abs(real(NNdisps)) >= abs(imag(NNdisps)));
+NNidx.u = find(imag(NNdisps) > 0 & abs(real(NNdisps)) < abs(imag(NNdisps)));
+NNidx.d = find(imag(NNdisps) < 0 & abs(real(NNdisps)) < abs(imag(NNdisps)));
 
 %% Checking condition and validity
+% check how many pts were assigned to each NN position
+numinpos = structfun(@numel,NNidx);
+if max(numinpos) > 1
+    error(toomanyinPtError)
+end
 
+% if one of the NN positions was not filled, assign it a dummy index
+unassignedidx = find(numinpos == 0);
+if numel(unassignedidx) > 0
+    for k = 1:numel(unassignedidx)
+        NNidx.(NNidxFields{unassignedidx(k)}) = numNN + 1;
+    end
+    NNdist(numNN + 1) = NaN;
+    NNang(numNN + 1) = NaN;
+end
+
+% check if any were assigned more than once, ignoring the dummy index
+allNNidx = structfun(@single,NNidx);
+tmpNNidx = allNNidx;
+tmpNNidx(allNNidx == numNN + 1) = []; % remove dummy indices
+if ~isequal(unique(tmpNNidx),sort(tmpNNidx))
+    error(repeatePtError);
+end
+
+%% Return Values
+NNdists = NNdist(allNNidx);
+NNangs = NNang(allNNidx);
+resortIdx = allNNidx;
+% resortIdx(resortIdx == numNN + 1) = 0;
 
