d7cbb4dbc4d2083b17a159d133390ea138e467e9
rib1 <rib1@DBH-133-opti9020_1.ad.rice.edu>
2017-06-13 15:22:30 -0500

    Switched to complex numbers to deal with nearest-neighbor sorting.

--- getNNparams.m
+++ getNNparams.m
@@ -4,4 +4,11 @@ function [  ] = getNNparams( centerpt, ptslist )
 %           ptslist - list of [x,y] row vectors of nearest-neighbors
 numNN = size(ptslist,1);
 NNdisps = ptslist - repmat(centerpt,numNN,1);
-disp(NNdisps)
+% Convert to complex number to make angle stuff easier
+NNdisps = NNdisps(:,1)+1i*NNdisps(:,2);
+NNdist = abs(NNdisps);
+NNang = angle(NNdisps);
+
+% Deal with everything in terms of indices for now
+lrNNs = find(abs(real(NNdisps)) >= abs(imag(NNdisps)));
+udNNs = find(abs(real(NNdisps)) < abs(imag(NNdisps)));
