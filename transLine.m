edd2d56f03315d48dec114954c626589f72d808f
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-14 13:10:26 -0500

    Updated transLine to accept multiple betas of [slope;intercept] form as a 2xM matrix of M lines.

--- transLine.m
+++ transLine.m
@@ -1,10 +1,13 @@
 function [ beta1 ] = transLine( beta0 )
-%transLine for an input [slope,intercept], return the transposed
-%[slope,intercept]
-m0 = beta0(1);
-b0 = beta0(2);
+%transLine for an input [slope;intercept], return the transposed
+%[slope;intercept] (column vectors)
+beta1 = zeros(size(beta0));
+for k = 1:size(beta0,2)
+    m0 = beta0(1,k);
+    b0 = beta0(2,k);
 
-m1 = 1/m0;
-b1 = -b0/m0;
+    m1 = 1/m0;
+    b1 = -b0/m0;
 
-beta1 = [ m1 , b1 ];
+    beta1(:,k) = [ m1 ; b1 ];
+end
