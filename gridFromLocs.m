0ec782fb402a5d9ff007edf84acb470c68618899
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-14 18:33:35 -0500

    Function gridFromLocs serves to generate the intersecting sets of parallel lines used to pick regions of interest for refined pore localization.

--- gridFromLocs.m
+++ gridFromLocs.m
@@ -7,5 +7,36 @@ function [ hLines , vLines ] = gridFromLocs( porelocs , nn_seprange )
 %               neighbors from the rif-raf.
 %   OUTPUT: hLines(vLines) - the sets of parallel lines that are close to
 %               horizontal(vertical) to initialize the grid.
+N = size(porelocs,1); % number of localizations
+% Get displacements and expected nearest-neighbors distance range
+dispmat = genDispmat( porelocs );
+nnmu = mean(nn_seprange);
+nnrange = nnmu-nn_seprange(1);
+% Filter down to just nearest neighbors
+nnBinary = abs(dispmat - nnmu) < nnrange;
+numnn = sum(nnBinary);
 
+% Calculating nearest-neighbor angles and distances for each point. Use
+% average angle for each of the four points on a cross (r,l,u,d) to figure
+% out the best angle estimates.
+NNdists = zeros(4,N);
+NNangs = zeros(4,N);
+for k = 1:N % Loop over each pore localization
+    centerpt = porelocs(k,:);
+    ptslist = porelocs(find(nnBinary(:,k)),:);
+    [NNdists(:,k),NNangs(:,k)] = getNNparams(centerpt,ptslist);
+end
+NNang = nanmean(NNangs,2);
 
+%% Calculating lines
+% Calculate slope and intercept for first horizontal and vertical line
+kk = find(numnn==4,1); % Use first pore with 4 nearest neighbors as point
+Hslope = cot(-mean(NNang(1:2)));
+Hint = ptslopeform(Hslope,porelocs(kk,:));
+Vslope = cot(-mean(NNang(3:4)));
+Vint = ptslopeform(Vslope,porelocs(kk,:));
+% Use initial lines to calculate the full array of lines. Vertical lines
+% are transposed, calculated, and transposed back to ease calculation.
+Hlines = calcParaLines([Hslope;Hint],porelocs);
+Vlines = calcParaLines(transLine([Vslope;Vint]),fliplr(porelocs));
+Vlines = transLine(Vlines);
