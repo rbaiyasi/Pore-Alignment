577b166963f1e639d2a5123080949b164029b293
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-16 17:13:16 -0500

    Completed function isClosedPointsAboutCenter.

--- isClosedPointsAboutCenter.m
+++ isClosedPointsAboutCenter.m
@@ -3,4 +3,20 @@ function [ closedflag ] = isClosedPointsAboutCenter( ptlist , xy0 )
 %origin whether they form a closed, circular shape.
 %   INPUT:  ptlist - Nx2 matrix of [x,y] row vectors describing the points.
 %           xy0 - [x,y] row vector of the center coordinate.
+N = size(ptlist,1);
 
+relr = ptlist - repmat(xy0,N,1);
+relr = relr(:,1) + 1i*relr(:,2); % convert to complex coords
+relang = wrapTo2Pi(angle(relr)); % get angle from center pixel
+[~,sortidx] = sort(relang);
+ptlist = ptlist(sortidx,:); % Sort coordinates by angle
+ptlist(N+1,:) = ptlist(1,:); % Add last coordinate to end for closed
+% Calculate distances between all adjacent points
+tmpdisp = ptlist(2:end,:) - ptlist(1:end-1,:);
+tmpdisp = sum(tmpdisp.^2,2);
+% Compare squared distance to maximum allowed (2)
+if max(tmpdisp) <= 2
+    closedflag = true;
+else
+    closedflag = false;
+end
