d40b9c20f123c6cc1740d51d5eb44bdbc87c31a4
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-19 18:50:57 -0500

    Functions well for dataset 14, but fails miserably for the others. Not sure why - perhaps due to uneven background.

--- testAFMwithConvScript.m
+++ testAFMwithConvScript.m
@@ -18,13 +18,49 @@ img_AFM = mean(img_AFM,3);
 [porelocs1 , filterParams] = filterByNN(pore_locs);
 
 %% get porelocs2 using convolution
+%estimate radius
+for k = 1:CC.NumObjects
+    [y,x] = ind2sub(CC.ImageSize,CC.PixelIdxList{k});
+    [xc,yc,tmpR] = circfit(x,y);
+    xy0(k,:) = [xc,yc];
+    R(k) = tmpR;
+end
+rad_est = median(R);
+% Generate unit circle
+numcircpts = 500;
+thetas = linspace(0,2*pi,numcircpts+1);
+thetas = thetas(1:(end-1));
+xs = cos(thetas); ys = sin(thetas);
+% create convolution kernel
+rad1 = rad_est*0.9;
+boxrad = round(rad1*2);
+boxres = ceil(2*boxrad+1); % size of img_test1
+r0 = [0,0]+boxres/2+0.5; % center of img_test1
+numcircpts = 100; % Number of angular coords
+xs1 = round(rad1*xs+r0(1)); ys1 = round(rad1*ys+r0(2));
+lininds1 = sub2ind(boxres*[1,1],ys1,xs1);
+img_test1 = false(boxres);
+img_test1(lininds1) = true;
+img_test1 = imfill(img_test1,round([1,1]*boxres/2));
+img_test1 = img_test1 * -1; %invert since it is negative down
 
-
+bg = imopen(img_AFM,strel('disk',round(rad1*10)));
+img_AFM2 = img_AFM - bg;
+convim1 = conv2(img_AFM2,img_test1); % Convolve first time
+convim1 = wkeep(convim1,size(img_AFM)); % Trim down size
+lminds = find_locmax(convim1,round(rad1/2),0.5);
+[ys,xs] = ind2sub(size(img_AFM),lminds);
 %% view Filter results
-init_porelocs = pore_locs;
-final_porelocs = porelocs1;
-figure(1); imagesc(img_AFM); axis image
+figure(1)
+imagesc(img_AFM);
+axis image
 hold on
-scatter(init_porelocs(:,1),init_porelocs(:,2),50,'or')
-scatter(final_porelocs(:,1),final_porelocs(:,2),50,'+k')
+scatter(xs,ys,50,'+k')
 hold off
+% init_porelocs = pore_locs;
+% final_porelocs = porelocs1;
+% figure(1); imagesc(img_AFM); axis image
+% hold on
+% scatter(init_porelocs(:,1),init_porelocs(:,2),50,'or')
+% scatter(final_porelocs(:,1),final_porelocs(:,2),50,'+k')
+% hold off
