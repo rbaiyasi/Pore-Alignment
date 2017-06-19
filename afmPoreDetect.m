cb248d69b22fd235311d3b441cf766f4efee509e
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-19 13:44:11 -0500

    Added afmPoreDetect function. Still under construction, though it functions for a first pass.

---
+++ afmPoreDetect.m
@@ -0,0 +1,31 @@
+function [ pore_locs , nn_seprange ] = afmPoreDetect( img_AFM )
+%bfPoreDetect Uses input afm image and to pick out the most obvious pores 
+%and return their locations.
+%   INPUT:  img_AFM - cropped afm image
+%   OUTPUT: pore_locs - estimated pore centers for pores picked out by
+%               convolution listed as [x,y] row vectors
+
+boundimg = edge(img_AFM,'Canny' , [] , sqrt(2));
+CC = bwconncomp(boundimg);
+blankimg = zeros(CC.ImageSize);
+porelocs1 = zeros(CC.NumObjects,2);
+for k = 1:CC.NumObjects
+    tmpimg = blankimg;
+    tmpimg(CC.PixelIdxList{k}) = 1;
+    cpx = calcCofMass(tmpimg);
+    [tmpy,tmpx] = ind2sub(CC.ImageSize,CC.PixelIdxList{k});
+    if numel(CC.PixelIdxList{k}) < 10
+        CC.closed(k) = false;
+    else
+        CC.closed(k) = isClosedPointsAboutCenter([tmpx,tmpy],cpx);
+    end
+    porelocs1(k,:) = cpx;
+end
+porelocs1 = porelocs1(CC.closed,:);
+CC.PixelIdxList(~CC.closed) = [];
+CC.NumObjects = numel(CC.PixelIdxList);
+CC.closed = true(1,CC.NumObjects);
+
+%% Return
+pore_locs = porelocs1;
+nn_seprange = [0,inf];
