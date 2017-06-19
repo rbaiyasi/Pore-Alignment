b302134be7b0ca08b6d4a72c9805e9d5bdb90117
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-19 16:40:55 -0500

    Changed filter determination to a median of minimum separations rather than extractBGstats. Standard deviation arbitrarily set to 0.10 of median.

--- filterByNN.m
+++ filterByNN.m
@@ -6,6 +6,25 @@ function [ outptlist , filterParams ] = filterByNN( ptlist )
 %               can be extracted.
 N = size(ptlist,1);
 
+% Convert to complex coordinates.
+pts = ptlist(:,1) + 1i*ptlist(:,2);
+dispmat = genDispmat(pts);
+% Use the median of the smallest separations between points to estimate
+% nearest-neighbor separations.
+sepmat = abs(dispmat);
+sortsepmat = sort(sepmat,1);
+minseps = sortsepmat(1,:);
+% Using median of distributed minimum steps and 0.10 of the median as the
+% standard deviation to extract appropriate points.
+mu3 = median(minseps);
+sig3 = 0.1*mu3;
+std_fact = 1;
 
-end
-
+mu = mu3;
+sig = sig3;
+nnmat = sum(abs(sepmat-mu) <= std_fact*sig);
+pts2keep = nnmat > 1; % needs at least two nearest neighbors;
+%% Return
+outptlist = ptlist(pts2keep,:);
+filterParams.mu = mu;
+filterParams.sigma = sig;
