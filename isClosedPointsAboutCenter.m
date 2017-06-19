b1f01c323a9711285d97633699221e18d01982a1
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-19 13:51:33 -0500

    Added gapParam to isClosePointsAboutCenter function. Enables openings in a circular edge to count as closed if they are small enough.

--- isClosedPointsAboutCenter.m
+++ isClosedPointsAboutCenter.m
@@ -1,8 +1,13 @@
-function [ closedflag ] = isClosedPointsAboutCenter( ptlist , xy0 )
+function [ closedflag ] = isClosedPointsAboutCenter( ptlist , xy0 , gapParam )
 %isClosedPointsAboutCenter Calculates for a set of points about a defined
 %origin whether they form a closed, circular shape.
 %   INPUT:  ptlist - Nx2 matrix of [x,y] row vectors describing the points.
 %           xy0 - [x,y] row vector of the center coordinate.
+%           gapParam - Defaults to 2. Square of distance between pixels for
+%               them to be considered adjacent. 
+if nargin < 3
+    gapParam = 2;
+end
 N = size(ptlist,1);
 
 relr = ptlist - repmat(xy0,N,1);
@@ -21,7 +26,7 @@ ptlist(N+1,:) = ptlist(1,:); % Add last coordinate to end for closed
 tmpdisp = ptlist(2:end,:) - ptlist(1:end-1,:);
 tmpdisp = sum(tmpdisp.^2,2);
 % Compare squared distance to maximum allowed (2)
-if max(tmpdisp) <= 2
+if max(tmpdisp) <= gapParam
     closedflag = true;
 else
     closedflag = false;
