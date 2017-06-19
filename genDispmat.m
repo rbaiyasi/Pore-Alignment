be44a03b81ca882dca0e412ac2d6f7745bde895a
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-19 14:58:11 -0500

    Updated genDispmat function to allow for obtaining the x and y separations.

--- genDispmat.m
+++ genDispmat.m
@@ -1,18 +1,21 @@
-function [ dispmat ] = genDispmat( ptslist )
+function [ dispmat , xdispmat , ydispmat ] = genDispmat( ptslist )
 %pointSeparations Takes and input of row-vector points and creates a
 %displacement map to be used for nearest-neigbor calculations and the like.
 %   INPUT:  ptslist - MxD matrix, where M is the number of points and D is
 %               the dimensionality of the data.
 %   OUTPUT: dispmat - MxM matrix of displacements between each points. The
 %               diagonals are set to zero to avoid the minimum of zero.
+%           xdispmat(ydispmat) - MxM matrix of x(y) separations between
+%               points. Diagonals are set to NaN.
 x0s = ptslist(:,1);
 y0s = ptslist(:,2);
-
-% Distances between all points
+N = numel(x0s);
+% Distances between all points. Ignore self-distance of zero
 xxmat = repmat(x0s,1,numel(x0s));
 yymat = repmat(y0s,1,numel(y0s));
-xdiff2 = (xxmat - xxmat').^2;
-ydiff2 = (yymat - yymat').^2;
-dispmat = sqrt(xdiff2+ydiff2);
-dispmat(dispmat == 0 ) = NaN; % Ignore self-distance of zero
+xdispmat = abs(xxmat - xxmat');
+xdispmat = xdispmat * eye(N)*NaN;
+ydispmat = abs(yymat - yymat');
+ydispmat = ydispmat * eye(N)*NaN;
+dispmat = sqrt(xdispmat.^2+ydispmat.^2);
 
