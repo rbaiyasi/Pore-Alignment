c3a223215c1a9dba0d915e469d6077becf625b56
Rashad I Baiyasi <rib1@ad.rice.edu>
2017-06-19 15:09:02 -0500

    Added function filterByNN to filter a set of points based on estimation of nearest-neighbor separations. Still under construction. Meant to povide a regular framework for nearest-neighbor filtering.

---
+++ filterByNN.m
@@ -0,0 +1,11 @@
+function [ outptlist , filterParams ] = filterByNN( ptlist )
+%filterByNN Takes an input set of points and filters out the ones that
+%don't fit into a grid-like scheme.
+%   INPUT:  ptlist - Nx2 array or [x,y] row vectors. It is assumed that
+%               there is enough consistency that the regularity of the map
+%               can be extracted.
+N = size(ptlist,1);
+
+
+end
+
