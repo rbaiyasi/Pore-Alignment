2f96b9b929ff66d31e1242221d6b8cbe6c1110f5
rib1 <rib1@DBH-133-opti9020_1.ad.rice.edu>
2017-06-21 14:34:48 -0500

    Testing AFM detection through imfindcircles as well as looking into AFM rotation.

---
+++ afmDetectTestScript.m
@@ -0,0 +1,45 @@
+%% Space to work out how to detect the brightfield pores.
+afmPath = 'Z:\Files for Rashad\Correlated_AFM_BF_images';
+[afmFileName , PathName] = uigetfile('AFM_*.tif','Choose AFM image',afmPath);
+if ~afmFileName
+    error('No AFM selected');
+end
+img_afm = imread([PathName,afmFileName]);
+img_afm = mean(img_afm,3);
+ul_afm = [69,73];
+lr_afm = [1092,1096];
+img_afm_crop = crop(img_afm,ul_afm,lr_afm);
+img_afm_crop = img_afm_crop'; % transpose for lining up
+img_afm = img_afm';
+
+fig1 = figure(1);
+imh = imagesc(img_afm_crop); axis image off
+ax1 = gca;
+setFont(16)
+
+% rh = imrect(ax1);
+% set(fig1,'KeyPressFcn',@continueOnReturn);
+
+% rad_est = 18; %1
+% rad_est = 28; %2
+% rad_est = 30; %3
+% rad_est = 34; %4 FAIL
+% rad_est = 35; %5 FAIL
+% rad_est = 38; %6 FAIL
+% rad_est = 38; %7 FAIL
+% rad_est = 36; %8 FAIL
+% rad_est = 29; %9 FAIL
+% rad_est = 19; %10 FAIL
+% rad_est = 13; %11 FAIL
+% rad_est = 14; %12
+% rad_est = 29; %13
+% rad_est = 37; %14
+% rad_est = 39; %15 FAIL
+% rad_est = 25; %16
+rad_est = porePicker(ax1)
+[afmporelocs1, radii_afm, metric_afm] = imfindcircles(img_afm_crop,...
+    round(rad_est + [-1,1]*8),'ObjectPolarity','dark');
+porelocs1 = afmporelocs1;
+N = size(porelocs1,1);
+figure(1)
+viscircles(porelocs1,rad_est*ones(N,1));
--- testScriptAFMandBF.m
+++ testScriptAFMandBF.m
@@ -4,7 +4,7 @@ bfPath = afmPath;
 PathName = [afmPath,'\'];
 afmFileName = 'AFM_area1.tif';
 % bfFileName = 'Bf_area1.tif';
-[afmFileName , PathName] = uigetfile('*.tif','Choose AFM image',afmPath);
+[afmFileName , PathName] = uigetfile('AFM_*.tif','Choose AFM image',afmPath);
 
 if ~afmFileName
     error('No AFM selected');
@@ -13,9 +13,10 @@ img_afm = imread([PathName,afmFileName]);
 img_afm = mean(img_afm,3);
 ul_afm = [69,73];
 lr_afm = [1092,1096];
-img_afm = crop(img_afm,ul_afm,lr_afm);
-
-[bfFileName , PathName] = uigetfile('*.tif','Choose BF image',bfPath);
+img_afm_crop = crop(img_afm,ul_afm,lr_afm);
+img_afm_crop = img_afm_crop'; % transpose for lining up
+img_afm = img_afm';
+[bfFileName , PathName] = uigetfile('BF_*.tif','Choose BF image',bfPath);
 if ~bfFileName
     error('No AFM selected');
 end
@@ -23,35 +24,39 @@ img_bf = imread([PathName,bfFileName]);
 img_bf = mean(img_bf,3);
 ul_bf = [141,210];
 lr_bf = [657,861];
-img_bf = crop(img_bf,ul_bf,lr_bf);
+img_bf_crop = crop(img_bf,ul_bf,lr_bf);
 
 figure(1)
 setFont(16)
 subplot(1,2,1)
-imagesc(img_afm);
+imagesc(img_afm_crop);
 ax_afm = gca;
 axis image
 title('AFM')
 
 subplot(1,2,2)
-imagesc(img_bf);
+imagesc(img_bf_crop);
 ax_bf = gca;
 axis image
 title('BF')
 %% get AFM initial pores
 Rs = porePicker(ax_afm);
 rad_est_afm = Rs;
-[afmporelocs1, radii_afm, metric_afm] = imfindcircles(img_afm,...
-    round(rad_est_afm + [-1,1]*5),'ObjectPolarity','dark');
-hold on
-s_afm = scatter(afmporelocs1(:,1),afmporelocs1(:,2),'+r','Parent',ax_afm);
-hold off
+[afmporelocs1, radii_afm, metric_afm] = imfindcircles(img_afm_crop,...
+    round(rad_est_afm + [-1,1]*10),'ObjectPolarity','dark');
+if ~isempty(afmporelocs1)
+    hold on
+    s_afm = scatter(afmporelocs1(:,1),afmporelocs1(:,2),'+r','Parent',ax_afm);
+    hold off
+else
+    disp('No AFM pores found')
+end
 
 %% get BF initial pores
 Rs = porePicker(ax_bf);
 radfact = 1.0;
 rad_est_bf = Rs * radfact;
-[ bfporelocs1 , nn_seprange ] = bfPoreDetect( img_bf , (rad_est_bf) );
+[ bfporelocs1 , nn_seprange ] = bfPoreDetect( img_bf_crop , (rad_est_bf) );
 N_bf = size(bfporelocs1,1);
 hold on
 s_bf = scatter(bfporelocs1(:,1),bfporelocs1(:,2),'+r','Parent',ax_bf);
