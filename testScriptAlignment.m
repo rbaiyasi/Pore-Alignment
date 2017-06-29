6dd1054e3ca5f0175f7028f1fc96eba4d786a9eb
rib1 <rib1@DBH-133-opti9020_1.ad.rice.edu>
2017-06-29 18:03:23 -0500

    Implemented alignment of regions determined from AFM on top of BF images. Improved alignment still needed.

--- testScriptAlignment.m
+++ testScriptAlignment.m
@@ -1,19 +1,106 @@
 %% Load in Data
-ogPath = 'C:\Users\rib1.ADRICE\Documents\PoreAdsorbtion\RawData\AFM_images\';
-PFXS = {'AFM','BF'};
-delim = '_';
-% choose afm file
-[afmFileName , PathName] = uigetfile('AFM*analysis.mat','Choose file',ogPath);
-% parse filename to get bf filename
-filename = strsplit(afmFileName,delim);
-filename = filename{end-1};
-bfFileName = [PFXS{2},delim,filename,delim,'analysis'];
-% Load data as structures
-AFM = load([PathName,afmFileName]);
-BF = load([PathName,bfFileName]);
+% ogPath = 'C:\Users\rib1.ADRICE\Documents\PoreAdsorbtion\RawData\AFM_images\';
+% PFXS = {'AFM','BF'};
+% delim = '_';
+% % choose afm file
+% [afmFileName , PathName] = uigetfile('AFM*analysis.mat','Choose file',ogPath);
+% % parse filename to get bf filename
+% filename = strsplit(afmFileName,delim);
+% filename = filename{end-1};
+% bfFileName = [PFXS{2},delim,filename,delim,'analysis'];
+% % Load data as structures
+% AFM = load([PathName,afmFileName]);
+% BF = load([PathName,bfFileName]);
 
 %% 
-figure(1)
+afmfig = figure(1);
 imagesc(AFM.Data1); axis image
-figure(2)
+hold on
+scatter(AFM.porelocs(:,1),AFM.porelocs(:,2),50,'+r');
+hold off
+bffig = figure(2);
 imagesc(BF.Data1); axis image
+hold on
+scatter(BF.porelocs(:,1),BF.porelocs(:,2),50,'+r');
+hold off
+
+%% AFM analysis
+% Getting boxsize needs to be improved.
+clocs = AFM.porelocs;
+croprad = round(norm(clocs(1,:) - clocs(2,:))/2 - 1);
+boxsize = 2*croprad + 1;
+uls = max(1,round(clocs - croprad));
+poreims = zeros(boxsize,boxsize,size(uls,1));
+for k = 1:size(uls,1)
+    poreims(:,:,k) = croprect(AFM.Data1,[uls(k,:),boxsize*[1,1]]);
+end
+
+outbound = cell(1,size(poreims,3));
+inbound = outbound;
+for k = 1:size(poreims,3)
+    [ outbound{k} , inbound{k} ] = genGradBounds(poreims(:,:,k));
+    outbound{k} = outbound{k} + uls(k,:) - 1;
+    inbound{k} = inbound{k} + uls(k,:) - 1;
+end  
+figure(afmfig);
+hold on
+for k = 1:numel(outbound)
+    pts1 = outbound{k};
+    pts2 = inbound{k};
+    plot(pts1(:,1),pts1(:,2),'r')
+    plot(pts2(:,1),pts2(:,2),'m')
+end
+clearvars pts1 pts2
+hold off
+%% Grid Alignment
+activepts = ~isnan(sum(AFM.porelocs,2)) & ~isnan(sum(BF.porelocs,2));
+
+afmlocs = AFM.porelocs(activepts,:);
+bflocs = BF.porelocs(activepts,:);
+
+[ finalPs , tfParams , MSEs , theta_hats ] = alignGrids( bflocs , afmlocs );
+tform = fitgeotrans(afmlocs([1,end],:),bflocs([1,end],:),'nonreflectivesimilarity');
+nout = outbound;
+nin = inbound;
+nout2 = nout;
+nin2 = nin;
+rotmat = @(ttt) [cos(ttt),-sin(ttt); sin(ttt),cos(ttt)];
+% Using my fit
+rr = rotmat(tfParams.phi);
+x0 = tfParams.x0;
+y0 = tfParams.y0;
+% using results of fitgeotrans - Not yet working
+% rr = tform.T(1:2,1:2);
+% x0 = tform.T(3);
+% y0 = tform.T(6);
+
+for k = 1:numel(nout)
+    pts1 = outbound{k};
+    pts2 = inbound{k};
+    pts1 = (tfParams.c*rr*pts1')';
+    pts1(:,1) = pts1(:,1) + tfParams.x0;
+    pts1(:,2) = pts1(:,2) + tfParams.y0;
+    nout{k} = pts1;
+    pts2 = (tfParams.c*rr*pts2')';
+    pts2(:,1) = pts2(:,1) + tfParams.x0;
+    pts2(:,2) = pts2(:,2) + tfParams.y0;
+    nin{k} = pts2;
+    
+%     tmp = outbound{k};
+%     tmp(:,3) = 1;
+%     tmp = tmp*tform.T;
+%     nout2{k} = tmp(1:2,:);
+%     tmp = inbound{k};
+%     tmp(:,3) = 1;
+%     tmp = tmp*tform.T;
+%     nin2{k} = tmp(1:2,:);
+end
+
+figure(bffig);
+hold on
+lnwdth = 2;
+for k = 1:numel(nout)
+    plot(nout{k}(:,1),nout{k}(:,2),'r','LineWidth',lnwdth);
+    plot(nin{k}(:,1),nin{k}(:,2),'m','LineWidth',lnwdth);
+end
+hold off
